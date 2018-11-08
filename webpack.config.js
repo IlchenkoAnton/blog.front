ce4777f4cf7356fe33f17f99529c0d286965071a
Ильченко Антон Александрович <a.ilchenko@watcom.ru>
2018-11-08 13:50:40 +0300

    Ностроено построение проекта. Установлены основные пакеты.

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            }, {
+                test: /\.css$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin(),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'src/index.html',
+            files: {
+                css: [
+                    "node_modules/bootstrap/dist/css/bootstrap.min.css"
+                ],
+                js: [
+                    "dist/bundle.js"
+                ]
+            }
+        })
+    ]
+};
