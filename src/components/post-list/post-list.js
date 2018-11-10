e094580c3484356592cc593e41d6500894e72988
Ильченко Антон Александрович <a.ilchenko@watcom.ru>
2018-11-11 00:46:04 +0300

    Добавлено отображение фейкового списка постов.

--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -1,6 +1,22 @@
+import axios from 'axios';
+
+import postListMock from './post-list.mock';
+
 export default {
     name: 'blog-post-list',
     data: function () {
-        return {};
+        return {
+            postList: null
+        };
+    },
+    mounted() {
+        axios
+            .get('url')
+            .then((response) => {
+                this.postList = response;
+            })
+            .catch((error) => {
+                this.postList = postListMock;
+            });
     }
 }
---
+++ src/components/post-list/post-list.mock.js
@@ -0,0 +1,27 @@
+export default [
+    {
+        id: 1,
+        title: 'Пост 1',
+        tags: ['авто', 'audi'],
+        date: '2018-11-11T00:26:48+03:00',
+        author: 'anton',
+        description: 'Краткое описание поста',
+        image: null
+    }, {
+        id: 2,
+        title: 'Пост 2',
+        tags: ['природа', 'прогулки'],
+        date: '2018-11-11T00:26:48+03:00',
+        author: 'anton',
+        description: 'Краткое описание поста',
+        image: null
+    }, {
+        id: 3,
+        title: 'Пост 3',
+        tags: ['новости', 'авто'],
+        date: '2018-11-11T00:26:48+03:00',
+        author: 'anton',
+        description: 'Краткое описание поста',
+        image: null
+    }
+];
